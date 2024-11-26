2e36c34ee5c0129fd3d506cdea3ffd1523283907
Jonathan Benn <bonjennprojects@gmail.com>
2024-11-26 15:24:49 -0800

    Added AI blogging infrastructure

--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -8,6 +8,7 @@ interface Props {
   params: {
     slug: string;
   };
+  searchParams: { [key: string]: string | string[] | undefined };
 }
 
 export async function generateMetadata({ params }: Props): Promise<Metadata> {
